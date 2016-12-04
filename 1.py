0a9b76dc54c61325b4390835130007b9b8b45fa3
greenbang89 <greenbang89@gmail.com>
2016-12-04 14:44:54 -0500

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,5 @@
+in_str = input("please write your password. \n")
+print(in_str.upper()+" world!")
+# \n = 한줄 띄어주기
+# hello / python / ruby
+# hello world/ python world/ ruby world
---
+++ 2.py
@@ -0,0 +1,21 @@
+#input = 22
+#real = 11
+#if real == input
+#  puts("hello!")
+#end
+
+#if real != input
+#  puts("who are you?")
+#end
+
+in_str = input ("Please print your id below. \n")
+# 아이디를 입력해주세요
+real_egoing = "11"
+# 숫자 11 과 문자 11을 비교햇기 대문에 11을 문자로 만들죠
+real_k8805 = "ab"
+if real_egoing == in_str:
+  print("hello!, egoing")
+elif real_k8805 == in_str:
+    print("hello k8805!")
+else:
+    print("who are you?")
